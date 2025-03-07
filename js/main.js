8f97c88a24f4f9dc25d097ed649e8568ddaa827d
pedro-delima <113381055+pedro-delima@users.noreply.github.com>
2025-03-07 14:29:36 -0300

    Update main.js
    bbas3 + 35

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const AcaoGerdau = 192
 const AcaoSanepar = 37
 const AcaoAuren = 157
 const AcaoTaesa = 71
-const AcaoBancoBrasil = 20
+const AcaoBancoBrasil = 55
 
 //nota de risco
 const notaItausa = 10
